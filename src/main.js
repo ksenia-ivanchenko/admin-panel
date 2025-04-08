f33da5bf6a525717d408e522c86d18826effc0b6
Ksenia Ivanchenko <k.ivanchenko20@gmail.com>
2025-04-08 16:41:06 +0700

    KAN-2 доработана конфигурация, удалены лишние файлы

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from 'react/jsx-runtime';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { StrictMode } from 'react';
-ReactDOM.render(
-  _jsx(StrictMode, { children: _jsx(App, {}) }),
-  document.getElementById('root')
-);
